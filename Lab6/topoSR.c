8ace0f8a20c7dab34693bd35adeef5c3836c87d7
PranayJaju <98613449+PranayJaju@users.noreply.github.com>
2023-03-09 02:00:46 +0530

    Add files via upload

---
+++ Lab6/topoSR.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"Queue.h"
+
+int indegree(int node, int** adjMat, int n)
+{
+    int indeg=0;
+    for(int i=0; i<n; i++)
+        if(*(*(adjMat+i)+node)==1)
+            indeg++;
+
+    return indeg;
+}
+
+void TopoSort(int** adjMat, int n)
+{
+    Queue* q=(Queue*)malloc(sizeof(Queue));
+    q->queue=(int*)calloc(2*n, sizeof(int));
+    q->front=-1;
+    q->rear=-1;
+    q->size=2*n;
+
+    int indeg[n]; int toposorted[n];
+    int count=0;
+
+    for(int i=0; i<n; i++)
+    {    
+        indeg[i]=indegree(i,adjMat, n);
+        if(indeg[i]==0)
+        enqueue(q,i);
+    }
+
+    while(!isEmpty(q))
+    {
+        int v=dequeue(q);
+        toposorted[count++]=v;
+        for(int i=0; i<n; i++)
+        {
+            if(*(*(adjMat+v)+i))
+            {
+                *(*(adjMat+v)+i)=0;
+                indeg[i]--;
+                if(indeg[i]==0) enqueue(q,i);
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++)
+        printf("%d  ", toposorted[i]);
+}
+
+int** AdjMat(int n)//taking adjacency matrix as input
+{
+	int** AM=(int**)malloc(sizeof(int*)*n);//array of pointers for adj matrix
+	for(int i=0; i<n; i++)
+		*(AM+i)=(int*)calloc(n,sizeof(int));//initializing individual pointers of adj matrix
+	
+	printf("Enter Adjacent Matrix.\n");//taking input of adj matrix
+	for(int i=0; i<n; i++)
+		for(int j=0; j<n; j++)
+			scanf("%d", *(AM+i)+j);
+
+	return AM;
+}
+
+int main()
+{
+    int n;
+	printf("Enter no.of vertices.\n");
+	scanf("%d", &n);
+	int** AM=AdjMat(n);
+
+    printf("Topological sort of given graph: ");
+    TopoSort(AM, n);
+}
