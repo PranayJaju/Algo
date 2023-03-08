d0448f70d966582f466b9582a99926b10d6abb5a
PranayJaju <98613449+PranayJaju@users.noreply.github.com>
2023-03-09 02:08:37 +0530

    Update topoSR.c

--- Lab6/topoSR.c
+++ Lab6/topoSR.c
@@ -1,19 +1,22 @@
+//Topological Sorting using Source Removal Method
+
 #include<stdio.h>
 #include<stdlib.h>
 #include"Queue.h"
 
+//to get the indegree of a node
 int indegree(int node, int** adjMat, int n)
 {
     int indeg=0;
     for(int i=0; i<n; i++)
         if(*(*(adjMat+i)+node)==1)
             indeg++;
-
     return indeg;
 }
 
 void TopoSort(int** adjMat, int n)
 {
+    //initializing queue
     Queue* q=(Queue*)malloc(sizeof(Queue));
     q->queue=(int*)calloc(2*n, sizeof(int));
     q->front=-1;
@@ -21,25 +24,25 @@ void TopoSort(int** adjMat, int n)
     q->size=2*n;
 
     int indeg[n]; int toposorted[n];
-    int count=0;
+    int count=0;//count of visited vertices
 
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++)//getting indegree of every node
     {    
         indeg[i]=indegree(i,adjMat, n);
-        if(indeg[i]==0)
+        if(indeg[i]==0)//if indegree is 0, add to queue
         enqueue(q,i);
     }
 
     while(!isEmpty(q))
     {
         int v=dequeue(q);
-        toposorted[count++]=v;
+        toposorted[count++]=v;//adding v to topological sorted array
         for(int i=0; i<n; i++)
         {
             if(*(*(adjMat+v)+i))
             {
-                *(*(adjMat+v)+i)=0;
-                indeg[i]--;
+                *(*(adjMat+v)+i)=0;//for node v, removing edges to neighbour
+                indeg[i]--;//decreasing neighbour's indegree
                 if(indeg[i]==0) enqueue(q,i);
             }
         }
